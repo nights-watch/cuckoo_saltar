199f921a8646157999497eb5cd037079b2ac72b8
Hialo Muniz <hialomuniz@gmail.com>
2015-06-18 16:24:04 -0300

    Splitting the main script and adding the clean_analysis

--- scripts/script.sh
+++ scripts/script.sh
@@ -32,12 +32,6 @@ PYTHON=python
 SERVICE=cuckoo
 TIME=$(date +"%T")
 
-GITCUCKOO="gitCuckooCommunity"
-GITYARA="gitYaraCommunity"
-
-GITCUCKOO_ADDR="https://github.com/cuckoobox/community.git"
-GITYARA_ADDR="https://github.com/citizenlab/malware-signatures.git"
-
 ##############################################################
 
 function startingVMs(){
@@ -45,7 +39,7 @@ function startingVMs(){
 
 	if [ $? -ne 0 ]; then
 		echo -e $TIME " Non-Active VMs. Running..."
-			vboxmanage startvm $VMNAME"1" --type vrdp #>> $LOG_PATH/$LOGFILE $DATA_STREAM
+			vboxmanage startvm $VMNAME"1" --type headless #>> $LOG_PATH/$LOGFILE $DATA_STREAM
 		#for i in {1..3}
 		#do
 			#vboxmanage startvm $VMNAME$i --type headless >> $LOG_PATH/$LOGFILE $DATA_STREAM
@@ -58,54 +52,6 @@ function startingVMs(){
 
 ##############################################################
 
-function syncCuckooSignatures(){
-	CUCKOO_SIG_DIR=/opt/$GITCUCKOO
-	CUCKOO_GIT_DIR=$CUCKOO_SIG_DIR/community
-
-	if [ ! -d $CUCKOO_SIG_DIR ]; then
-		echo -e $TIME " Cuckoo signatures directory doesn't exist. Creating..."
-		mkdir $CUCKOO_SIG_DIR -m 775
-
-		echo -e $TIME " Cloning remote repository..."
-		git clone $GITCUCKOO_ADDR $CUCKOO_SIG_DIR/community
-	fi
-
-	cd $CUCKOO_GIT_DIR
-
-	echo -e $TIME " Updating possible new cuckoo signatures..."
-	git pull
-
-	cp -rf $CUCKOO_GIT_DIR/modules/signatures/* /opt/cuckoo_saltar/modules/signatures/
-}
-
-##############################################################
-
-function syncYaraSignatures(){
-	YARA_SIG_DIR=/opt/$GITYARA
-
-	if [ ! -d $YARA_SIG_DIR ]; then
-		echo -e $TIME " YARA signatures directory doesn't exist. Creating..."
-		mkdir $YARA_SIG_DIR -m 775
-
-		echo -e $TIME " Cloning remote repository..."
-		git clone $GITYARA_ADDR $YARA_SIG_DIR
-	fi
-
-	cd $YARA_SIG_DIR
-
-	echo -e $TIME " Updating possible new YARA signatures..."
-	git pull
-
-	cp -rf $YARA_SIG_DIR/yara-rules/malware-families $CUCKOO_PATH/data/yara/
-
-	echo -e "include \"/opt/cuckoo_saltar/data/yara/binaries/shellcodes.yar\"" > /opt/cuckoo_saltar/data/yara/index_binaries.yar
-	echo -e "include \"/opt/cuckoo_saltar/data/yara/binaries/embedded.yar\"" >> /opt/cuckoo_saltar/data/yara/index_binaries.yar
-	echo -e "include \"/opt/cuckoo_saltar/data/yara/binaries/vmdetect.yar\"" >> /opt/cuckoo_saltar/data/yara/index_binaries.yar
-	echo -e "include \"/opt/cuckoo_saltar/data/yara/malware-families/\"" >> /opt/cuckoo_saltar/data/yara/index_binaries.yar
-}
-
-##############################################################
-
 if [ ! -d $LOG_PATH ]; then
 	echo -e $TIME " Log directory doesn't exist. Creating..."
 	mkdir $LOG_PATH -m 775
@@ -114,8 +60,6 @@ fi
 if ps ax | grep -i cuckoo | grep -v grep > /dev/null; then
     echo -e $TIME  " $SERVICE service is running already. Exiting...\n"
 else
-	syncCuckooSignatures
-	syncYaraSignatures
 	startingVMs
 	$PYTHON $CUCKOO_SCRIPT --clean
 	$PYTHON $SUBMIT_SCRIPT $MALWARE_PATH --enforce-timeout
---
+++ scripts/update_signatures.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Hialo Muniz, Vinicius Franco
+#  Description:
+#
+#		   This script checks if there's any VMs running, checks its
+#          status then executes the main script of cuckoo sandbox, with
+#		   a list of malwares contained in MALWARES_PATH. This script also 
+#		   checks if there is any new signatures in cuckoo's and yara's
+#		   repositories.
+#
+# Dependency:
+#     Writing permissions in /opt. Commands:
+#          sudo chown $USER:root -R /opt/
+#          sudo chmod 755 -R /opt/
+#          * * * * * cd /opt/cuckoo_saltar/scripts; export DISPLAY:=0 && nohup ./script.sh &
+#     iptables script already executed in the machine, necessary for the 
+#     VM network to work.
+# ------------------------------------------------------------------
+
+TIME=$(date +"%T")
+
+CUCKOO_PATH=/opt/cuckoo_saltar
+GITCUCKOO="gitCuckooCommunity"
+GITYARA="gitYaraCommunity"
+
+GITCUCKOO_ADDR="https://github.com/cuckoobox/community.git"
+GITYARA_ADDR="https://github.com/citizenlab/malware-signatures.git"
+
+##############################################################
+
+function syncCuckooSignatures(){
+	CUCKOO_SIG_DIR=/opt/signatures/$GITCUCKOO
+	CUCKOO_GIT_DIR=$CUCKOO_SIG_DIR/community
+
+	if [ ! -d $CUCKOO_SIG_DIR ]; then
+		echo -e $TIME " Cuckoo signatures directory doesn't exist. Creating..."
+		mkdir $CUCKOO_SIG_DIR -pm 775
+
+		echo -e $TIME " Cloning remote repository..."
+		git clone $GITCUCKOO_ADDR $CUCKOO_SIG_DIR/community
+	fi
+
+	cd $CUCKOO_GIT_DIR
+
+	echo -e $TIME " Updating possible new cuckoo signatures..."
+	git pull
+
+	cp -rf $CUCKOO_GIT_DIR/modules/signatures/* /opt/cuckoo_saltar/modules/signatures/
+}
+
+##############################################################
+
+function syncYaraSignatures(){
+	YARA_SIG_DIR=/opt/signatures/$GITYARA
+
+	if [ ! -d $YARA_SIG_DIR ]; then
+		echo -e $TIME " YARA signatures directory doesn't exist. Creating..."
+		mkdir $YARA_SIG_DIR -pm 775
+
+		echo -e $TIME " Cloning remote repository..."
+		git clone $GITYARA_ADDR $YARA_SIG_DIR
+	fi
+
+	cd $YARA_SIG_DIR
+
+	echo -e $TIME " Updating possible new YARA signatures..."
+	git pull
+
+	cp -rf $YARA_SIG_DIR/yara-rules/malware-families $CUCKOO_PATH/data/yara/
+
+	echo -e "include \"/opt/cuckoo_saltar/data/yara/binaries/shellcodes.yar\"" > /opt/cuckoo_saltar/data/yara/index_binaries.yar
+	echo -e "include \"/opt/cuckoo_saltar/data/yara/binaries/embedded.yar\"" >> /opt/cuckoo_saltar/data/yara/index_binaries.yar
+	echo -e "include \"/opt/cuckoo_saltar/data/yara/binaries/vmdetect.yar\"" >> /opt/cuckoo_saltar/data/yara/index_binaries.yar
+	echo -e "include \"/opt/cuckoo_saltar/data/yara/malware-families/\"" >> /opt/cuckoo_saltar/data/yara/index_binaries.yar
+
+}
+
+syncCuckooSignatures
+syncYaraSignatures
