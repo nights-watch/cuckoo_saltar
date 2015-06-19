8dc14eb53bfb2f1a41ca199fb733ffc131479e56
Claudio Rosa de Camargo Júnior <claudio.camargojr@gmail.com>
2015-06-19 11:17:44 -0300

    HTTP parser complet. Updated
    Testar

--- modules/processing/network_parser/http.py
+++ modules/processing/network_parser/http.py
@@ -12,8 +12,7 @@ except ImportError:
 
 class Http:
     def __init__(self):
-        self.http_package = [] # List containing package.
-        self.header = {} # Dictionary of header
+        pass
 
     @staticmethod
     def check(tcpdata, destinationPort):
@@ -33,7 +32,7 @@ class Http:
         return True
 
     @staticmethod
-    def dissect(self, data, destinationPort):
+    def dissect(data, destinationPort):
         """
         Realize parser of payload of protocol TCP identified like HTTP protocol
 
@@ -41,7 +40,8 @@ class Http:
         :return: Dictionary with the headers and payload of protocol HTTP
         """
 
-        header={}
+        http_package = [] # List containing package.
+        header = {} # Dictionary of header
 
         try:
             http = dpkt.http.Request()
@@ -50,15 +50,15 @@ class Http:
             pass
 
         try:
-            self.http_package["version"] = convert_to_printable(http.version) #Version of protocol HTTP in use
-            self.http_package["method"] = convert_to_printable(http.method) #Method of request HTTP
+            http_package["version"] = convert_to_printable(http.version) #Version of protocol HTTP in use
+            http_package["method"] = convert_to_printable(http.method) #Method of request HTTP
 
             # Parser HTTP headers, assuming it is not possible to determine the items in the header,
             # converting the data of each attribute to readable format
             for k in http.headers.iteritems():
-                self.header[k] = convert_to_printable(http.headers[k])
+                header[k] = convert_to_printable(http.headers[k])
 
-            self.http_package["header"]=header # Add Header HTTP converted to printable String
+            http_package["header"]= header # Add Header HTTP dictionary converted to printable String
 
             # Attribute HOST in header treatment
             # The host attribute is checked for the treatment of HTTP requests that do not use the standard port,
@@ -69,23 +69,23 @@ class Http:
                 header["host"] = ""
 
             # Destination port of TCP header
-            self.http_package["port"] = destinationPort
+            http_package["port"] = destinationPort
 
             # Manually deal with cases when destination port is not the default one,
             # and it is  not included in host header.
-            netloc = self.http_package["host"]
+            netloc = http_package["host"]
             if destinationPort != 80 and ":" not in netloc:
-                netloc += ":" + str(self.http_package["port"])
+                netloc += ":" + str(http_package["port"])
 
             # Mount the URI complete
-            self.http_package["uri"] = convert_to_printable(urlunparse(("http",
+            http_package["uri"] = convert_to_printable(urlunparse(("http",
                                                             netloc,
                                                             http.uri, None,
                                                             None, None)))
             # Body of request/response HTTP, considered the payload of request HTTP. according of RFC 2616
-            self.http_package["body"] = convert_to_printable(http.body)
+            http_package["body"] = convert_to_printable(http.body)
 
         except Exception:
             return False
 
-        return self.http_package
+        return http_package
