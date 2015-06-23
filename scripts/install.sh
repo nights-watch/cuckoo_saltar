4f9c3c1437089bd7e4ef4cbe844c6e8631e69efb
root <root@localhost.localdomain>
2015-06-23 11:49:44 -0300

    fixing install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get -y install python python-pip mongodb python-sqlalchemy python-jinja2 python-pymongo python-bottle python-pefile python-django python-chardet python-nose python-bson tcpdump libcap2-bin
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-sudo mv /opt/cuckoo_saltar/scripts/iptables /etc/init.d/
+sudo cp /opt/cuckoo_saltar/scripts/iptables /etc/init.d/
 sudo chmod 755 /etc/init.d/iptables
 sudo update-rc.d iptables defaults
