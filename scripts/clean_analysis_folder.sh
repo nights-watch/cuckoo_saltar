4f5465145e205b6c0f8f7b547e3a75cb91d15265
Igor Guimarães Dias <igonline15@gmail.com>
2015-06-23 15:49:57 -0300

    Reformated code and fixed some issues

--- scripts/clean_analysis_folder.sh
+++ scripts/clean_analysis_folder.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 # ------------------------------------------------------------------
 # [Author] Gustavo Correia
-#  Description:
-#               Cleans the analisys folder to delete unused files and folders.
+# [Co-author] Igor Guimarães Dias <igonline15@gmail.com>
+# Description:
+#               Cleans the analyses folder to delete unused files and folders.
 # ------------------------------------------------------------------
 
-cd ../storage
-rm -r binaries/
-cd analyses/
-find . -type d -exec sh -c '(cd {} && rm -r files/ logs/ shots/ analysis.log binary reports/report.html)' ';'
+CUCKOO_PATH="/opt/cuckoo_saltar"
+CUCKOO_ANALYSES="$CUCKOO_PATH/storage/analyses"
+
+rm -r $CUCKOO_PATH/storage/binaries/
+
+for dir in $CUCKOO_ANALYSES
+do
+    echo "Removing unused files and folders..."
+    rm -rf files/ logs/ shots/ analysis.log binary reports/report.html
+    echo "...done"
+done
+
+#find $CUCKOO_PATH/storage/analyses/ -type d -exec sh \
+# -c '(cd {} && rm -r files/ logs/ shots/ analysis.log binary reports/report.html)' ';'
 
 
